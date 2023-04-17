1ebb4382a5d0fef193942f8417fb5b1577025eb6
Ana <keeren.1996@gmail.com>
2023-04-17 13:18:08 -0300

    Atualização currículo portfólio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
-});
-
 function menuToggle() {
   const menuToggle = document.querySelector(".menuToggle");
   const navigation = document.querySelector(".navigation");
