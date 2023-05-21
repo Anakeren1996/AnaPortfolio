41b0f610448c38007da5e899016296dcbf2d48b2
Ana <keeren.1996@gmail.com>
2023-05-21 14:59:46 -0300

    Melhora da responsividade e acessibilidade

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-function menuToggle() {
-  const menuToggle = document.querySelector(".menuToggle");
-  const navigation = document.querySelector(".navigation");
-  menuToggle.classList.toggle("active");
-  navigation.classList.toggle("active");
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  const nav = document.getElementById("nav");
+  // ADICIONE CAO NÃO TENHA E REMOVA QUANDO TIVER A CLASSE
+  nav.classList.toggle("active");
+
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
 }
+btnMobile.addEventListener("click", toggleMenu);
+// PARA MELHORAR A ACESSIBILIDADE NO RESPONSIVO, TOUCHSTART
+btnMobile.addEventListener("touchstart", toggleMenu);
